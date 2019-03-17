2d025e40475b20aa9e71c0d084a15db64c6ca939
LEE SANG AH <elfish97@naver.com>
2019-03-17 22:44:13 +0900

    javaprac w2

---
+++ 0314javaprac_w2_animal/A_animal.java
@@ -0,0 +1,12 @@
+public class A_animal{
+    // instance vars
+    String food;
+    String location;
+    String status;
+    // methods
+    public void makeNoise(){}
+    public void eat(){}
+    public void sleep(){}
+    public void roam(){}
+
+}
---
+++ 0314javaprac_w2_animal/B_human.java
@@ -0,0 +1,6 @@
+public class B_human extends A_animal{
+    // new method 'only' for B_human
+    public void talking(){
+        System.out.println("human : blah blah");
+    }
+}
